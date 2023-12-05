3e8e885efc163b82d280d75e140078d9fda20c63
“412140” <412140@bsd48.org>
2023-12-05 03:01:47 +0000

    node header file

--- node.h
+++ node.h
@@ -1,7 +1,24 @@
 #ifndef NODE_H
 #define NODE_H
-
+#include "student.h"
 using namespace std;
+
+class Node 
+{
+    public:
+    //constructor
+    Node(Student* student);
+    //destructor
+    ~Node();
+    Node* getNext() const;
+    Student getStudent() const;
+    void setNext(Node* next);
+    private: 
+    Student* student;
+    Node* next;
+
+};
+
 
 
 #endif
