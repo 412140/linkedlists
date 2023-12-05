c57254127fbb98cff81d766c0324362dea7c1a0f
U-BSD\412140 <412140@SHS-26W2NW3.beaverton.k12.or.us>
2023-12-05 13:11:02 -0800

    node.cpp barebones

--- node.cpp
+++ node.cpp
@@ -1,3 +1,24 @@
 #include "node.h"
 #include <iostream>
 using namespace std;
+
+Node::Node(Student* student)
+{
+
+}
+Node::~Node()
+{
+
+}
+Node::Node* getnext()
+{
+
+}
+Node::Student getStudent() const
+{
+
+}
+Node::void setNext(Node* next)
+{
+
+}
